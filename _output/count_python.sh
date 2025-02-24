0e21bd66b6bf556ab479db1a257370460b171cb5
Ubuntu <ubuntu@ip-172-31-19-172.ec2.internal>
2025-02-24 15:37:05 +0000

    FIRST COMMIT

--- _output/count_python.sh
+++ _output/count_python.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-count=$(grep -i "python"  inflating: question_tags.csv | wc -l)
-echo "Number of lines containing 'python' in CSV files: $count"
+
+FILES=("questions.csv" "question_tags.csv")
+TOTAL_COUNT=0
+
+for FILE in "${FILES[@]}"; do
+
+    COUNT=$(grep -i "python" "$FILE" | wc -l)
+    TOTAL_COUNT=$((TOTAL_COUNT + COUNT))
+
+done
+
+echo "Total number of lines containing 'python' in all files: $TOTAL_COUNT"
 
